356472b6cb046c15f976f84a8dc8151cfe82199d
Kasra Bigdeli <kasra85@gmail.com>
2020-08-24 20:55:52 -0400

    Building NetData

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 # ensure you're not running it on local machine
-env
+if [ -z "$CI" ] || [ -z "$GITHUB_REF" ]; then
+    echo "Running on a local machine! Exiting!"
+    exit 127
+else
+    echo "Running on CI"
+fi
 
-
+exit 0
 # docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 MY_IMAGE_NAME="caprover/netdata"
 NETDATA_VERSION="v1.8.0"
